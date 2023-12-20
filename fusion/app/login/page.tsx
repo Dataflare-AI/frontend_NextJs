f397edc278fc1e663ef2fde67842e27ee44d8c96
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2023-12-20 16:37:09 -0300

    Add new files, update package dependencies, and modify UI styles

--- fusion/app/login/page.tsx
+++ fusion/app/login/page.tsx
@@ -3,11 +3,11 @@ import LoginForm from "@/app/ui/login-form";
 
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">{/* <AcmeLogo /> */}</div>
-        </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-br from-blue-500 to-white-600 text-black">
+      <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        {/* <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          // <div className="w-32 text-white md:w-36">{/* <AcmeLogo /> */}
+        {/* </div> */}
         <LoginForm />
       </div>
     </main>
