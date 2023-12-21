446ad30295dabe8b6014a1ed6d4e26d552b32c50
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2023-12-21 09:27:23 -0300

    Add dataSciencePython.jpeg and update login page layout

--- fusion/app/login/page.tsx
+++ fusion/app/login/page.tsx
@@ -1,13 +1,9 @@
-// import AcmeLogo from "@/app/ui/acme-logo";
 import LoginForm from "@/app/ui/login-form";
 
 export default function LoginPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-br from-blue-500 to-white-600 text-black">
-      <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-        {/* <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          // <div className="w-32 text-white md:w-36">{/* <AcmeLogo /> */}
-        {/* </div> */}
+    <main>
+      <div>
         <LoginForm />
       </div>
     </main>
--- fusion/app/ui/login-form.tsx
+++ fusion/app/ui/login-form.tsx
@@ -1,114 +1,94 @@
-"use client";
-
+import Image from "next/image";
+import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts";
-// import {
-//   AtSymbolIcon,
-//   KeyIcon,
-//   ExclamationCircleIcon,
-// } from "@heroicons/react/24/outline";
-// import { ArrowRightIcon } from "@heroicons/react/20/solid";
-import { Button } from "./button";
-import { useFormState, useFormStatus } from "react-dom";
-import { authenticate } from "@/app/lib/actions";
-// import "./globals.css";
 
 export default function LoginForm() {
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
-
   return (
-    <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
-      <div className="md:w-1/3 max-w-sm">
-        <img
-          src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-          alt="Sample image"
-        />
-      </div>
-      <div className="md:w-1/3 max-w-sm">
-        <div className="text-center md:text-left">
-          <label className="mr-1">Sign in with</label>
-          <button
-            type="button"
-            className="mx-1 h-9 w-9 rounded-full bg-blue-600 hover:bg-blue-700 text-white shadow-[0_4px_9px_-4px_#3b71ca]"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="mx-auto h-3.5 w-3.5"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
-            </svg>
-          </button>
-          <button
-            type="button"
-            className="inlne-block mx-1 h-9 w-9 rounded-full bg-blue-600 hover:bg-blue-700 uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca]"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="mx-auto h-3.5 w-3.5"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-            </svg>
-          </button>
-        </div>
-        <div className="my-5 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
-          <p className="mx-4 mb-0 text-center font-semibold text-slate-500">
-            Or
-          </p>
-        </div>
-        <input
-          className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-          type="password"
-          placeholder="Password"
-        />
-        <div className="mt-4 flex justify-between font-semibold text-sm">
-          <label className="flex text-slate-500 hover:text-slate-600 cursor-pointer">
-            <input className="mr-1" type="checkbox" />
-            <span>Remember Me</span>
-          </label>
-          <a
-            className="text-blue-600 hover:text-blue-700 hover:underline hover:underline-offset-4"
-            href="#"
-          >
-            Forgot Password?
-          </a>
-        </div>
-        <div className="text-center md:text-left">
-          <button
-            className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
-            type="submit"
-          >
-            Login
-          </button>
-        </div>
-        <div className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left">
-          Don't have an account?{" "}
-          <a
-            className="text-red-600 hover:underline hover:underline-offset-4"
-            href="#"
-          >
-            Register
-          </a>
+    <div className="flex flex-col items-center md:flex-row h-screen">
+      <div className="flex items-center justify-center w-full md:w-1/2 h-full">
+        <div className="w-full h-full relative overflow-hidden">
+          <Image
+            src="/dataSciencePython.jpeg"
+            alt="Login Image"
+            layout="fill"
+            objectFit="cover"
+          />
         </div>
       </div>
-    </section>
+      <div className="flex items-center justify-center w-full md:w-1/2 h-full bg-white text-black">
+        <div className="w-full max-w-md p-8">
+          <div className="text-center">
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              width={200}
+              height={80}
+              className="mx-auto mb-4"
+            />
+          </div>
+          <form className="space-y-3">
+            <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+              <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+                Please log in to continue.
+              </h1>
+              <div className="w-full">
+                <div>
+                  <label
+                    className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                    htmlFor="email"
+                  >
+                    Email
+                  </label>
+                  <div className="relative">
+                    <input
+                      className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                      id="email"
+                      type="email"
+                      name="email"
+                      placeholder="Enter your email address"
+                      required
+                    />
+                    {/* <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
+                  </div>
+                </div>
+                <div className="mt-4">
+                  <label
+                    className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                    htmlFor="password"
+                  >
+                    Password
+                  </label>
+                  <div className="relative">
+                    <input
+                      className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                      id="password"
+                      type="password"
+                      name="password"
+                      placeholder="Enter password"
+                      required
+                      minLength={6}
+                    />
+                    {/* <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
+                  </div>
+                </div>
+              </div>
+              {/* <LoginButton /> */}
+              <div
+                className="flex h-8 items-end space-x-1"
+                aria-live="polite"
+                aria-atomic="true"
+              >
+                {/* {errorMessage && (
+                  <>
+                    <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+                    <p className="text-sm text-red-500">{errorMessage}</p>
+                  </>
+                )} */}
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
-
-// function LoginButton() {
-//   const { pending } = useFormStatus();
-
-//   return (
-//     <Button className="mt-4 w-full" aria-disabled={pending}>
-//       Log-in
-//       {/* Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" /> */}
-//     </Button>
-//   );
-// }
