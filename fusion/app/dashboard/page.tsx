c97f885b95cf264dcf2a55a119a2237b077de40e
dudubarbaro <>
2023-12-27 16:50:20 -0300

    Refactor dashboard page with updated chart data

--- fusion/app/dashboard/page.tsx
+++ fusion/app/dashboard/page.tsx
@@ -2,31 +2,32 @@
 
 import { FaUpload, FaTable, FaComments } from "react-icons/fa";
 import Link from "next/link";
+import { useEffect } from "react";
 import Chart from "chart.js/auto";
 
 function App() {
-  // Dados de exemplo para o gráfico de barras
+  // Dados fictícios para o gráfico de barras
   const barChartData = {
     labels: ["Jan", "Feb", "Mar", "Apr", "May"],
     datasets: [
       {
-        label: "Example Data",
+        label: "Número de Projetos",
         backgroundColor: "rgba(75,192,192,0.2)",
         borderColor: "rgba(75,192,192,1)",
         borderWidth: 1,
         hoverBackgroundColor: "rgba(75,192,192,0.4)",
         hoverBorderColor: "rgba(75,192,192,1)",
-        data: [65, 59, 80, 81, 56],
+        data: [15, 22, 18, 25, 20],
       },
     ],
   };
 
-  // Dados de exemplo para o gráfico de pizza
+  // Dados fictícios para o gráfico de pizza
   const pieChartData = {
-    labels: ["Red", "Blue", "Yellow"],
+    labels: ["Machine Learning", "Data Visualization", "Data Analysis"],
     datasets: [
       {
-        data: [25, 30, 20],
+        data: [40, 30, 30],
         backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
         hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
       },
@@ -63,10 +64,15 @@ function App() {
     }
   };
 
+  useEffect(() => {
+    createBarChart(barChartData, "barChartCanvas");
+    createPieChart(pieChartData, "pieChartCanvas");
+  }, []); // Executar uma vez ao montar o componente
+
   return (
     <div className="wrapper p-8 bg-white">
       <h3 className="text-3xl font-bold mb-6 text-gray-800">
-        Dashboard de Administração
+        Dashboard de Análise de Dados em Python
       </h3>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
@@ -108,18 +114,18 @@ function App() {
       <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <div className="p-4 bg-gray-200 rounded-md">
           <canvas id="barChartCanvas" width="400" height="200"></canvas>
-          {createBarChart(barChartData, "barChartCanvas")}
         </div>
 
         <div className="p-4 bg-gray-200 rounded-md">
           <canvas id="pieChartCanvas" width="400" height="200"></canvas>
-          {createPieChart(pieChartData, "pieChartCanvas")}
         </div>
 
         <div className="p-4 bg-gray-200 rounded-md">
           <div className="text-xl font-bold mb-2">Dados Informativos</div>
-          <p>Quantidade de usuários ativos: 500</p>
-          <p>Receita total: R$ 100.000,00</p>
+          <p>Número total de projetos: 100</p>
+          <p>Projetos de Machine Learning: 40</p>
+          <p>Projetos de Data Visualization: 30</p>
+          <p>Projetos de Data Analysis: 30</p>
         </div>
       </div>
     </div>
