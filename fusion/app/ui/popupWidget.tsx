1a2424441937a21e6e52ab7364e85b9855fb989a
dudubarbaro <>
2023-12-28 13:21:44 -0300

    Update react-hook-form import in popupWidget.tsx

--- fusion/app/ui/popupWidget.tsx
+++ fusion/app/ui/popupWidget.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { useForm, useWatch } from "react-hook-form";
+import { useForm, useWatch, SubmitHandler } from "react-hook-form";
 import { Disclosure, Transition } from "@headlessui/react";
 
 interface FormData {
