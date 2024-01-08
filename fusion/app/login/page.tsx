a7e7c7bc1123bae7edfcc18dca9bcbb14a1aae5b
dudubarbaro <>
2024-01-08 19:26:21 -0300

    Update dashboard and login page, add ImportFiles component

--- fusion/app/login/page.tsx
+++ fusion/app/login/page.tsx
@@ -1,11 +1,12 @@
 import LoginForm from "@/app/ui/login-form";
+import { ThemeProvider } from "@/app/context/ThemeProvider";
 
 export default function LoginPage() {
   return (
     <main className="bg-white min-h-full">
-      <div>
+      <ThemeProvider>
         <LoginForm />
-      </div>
+      </ThemeProvider>
     </main>
   );
 }
