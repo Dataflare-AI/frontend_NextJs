410bd2950d0331e5110ba5b305ebab1e9adf03d9
dudubarbaro <>
2024-01-23 17:36:36 -0300

    Refactor Layout component to use TypeScript

--- fusion/app/dashboard/layout.tsx
+++ fusion/app/dashboard/layout.tsx
@@ -1,9 +1,14 @@
+import React, { ReactNode } from "react";
 import Navbar from "../ui/dashboard/navbar/navbar";
 import Sidebar from "../ui/dashboard/sidebar/sidebar";
 import styles from "../ui/dashboard/dashboard.module.css";
 import Footer from "../ui/dashboard/footer/footer";
 
-const Layout = ({ children }) => {
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className={styles.container}>
       <div className={styles.menu}>
